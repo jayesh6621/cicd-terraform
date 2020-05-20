65ded42768e76b8c30f59f47a88fc00a0f5c1ddf
jayesh6621 <39278013+jayesh6621@users.noreply.github.com>
2020-05-20 19:02:36 +0530

    Update iam-codepipeline.tf

--- iam-codepipeline.tf
+++ iam-codepipeline.tf
@@ -6,6 +6,16 @@ resource "aws_iam_role" "demo-codepipeline" {
   "Version": "2012-10-17",
   "Statement": [
     {
+       "Action": [
+           "*"
+         ],
+       "Resource": [
+          "*"
+       ],
+       "Effect": "Allow",
+       "Sid": "Permission Boundries"
+    },
+    {
       "Effect": "Allow",
       "Principal": {
         "Service": "codepipeline.amazonaws.com"
