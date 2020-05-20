120295987bb25c619e2415943e7502309981dd8d
jayesh6621 <39278013+jayesh6621@users.noreply.github.com>
2020-05-20 15:19:41 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "demo-artifacts" {
+  bucket = "demo-artifacts-${random_string.random.result}"
+  acl    = "private"
+
+  lifecycle_rule {
+    id      = "clean-up"
+    enabled = "true"
+
+    expiration {
+      days = 30
+    }
+  }
+}
+
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+}
