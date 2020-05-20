4f85f3ae8aabedc5a98571a14ed464a858bc496e
jayesh6621 <39278013+jayesh6621@users.noreply.github.com>
2020-05-20 15:13:35 +0530

    Create codepipeline.tf

---
+++ codepipeline.tf
@@ -0,0 +1,45 @@
+resource "aws_codepipeline" "demo" {
+  name     = "demo-docker-pipeline"
+  role_arn = aws_iam_role.demo-codepipeline.arn
+
+  artifact_store {
+    location = aws_s3_bucket.demo-artifacts.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeCommit"
+      version          = "1"
+      output_artifacts = ["demo-docker-source"]
+
+      configuration = {
+        RepositoryName = aws_codecommit_repository.demo.repository_name
+        BranchName     = "master"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["demo-docker-source"]
+      output_artifacts = ["demo-docker-build"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.demo.name
+      }
+    }
+  }
+}
